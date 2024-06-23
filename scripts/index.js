2a64d15dbed74544a0d56bf2f0fcd0ee19897ddd
Aditya kumar <138279499+Mirage-Re-Born-6002@users.noreply.github.com>
2024-06-23 15:41:30 +0530

    index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jun 20, 2024',
+  birthdate: 'Jun 24, 2024',
   name: 'Mom!'
   
 };
